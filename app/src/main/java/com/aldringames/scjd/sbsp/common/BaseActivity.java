464682a53d4ee64f55cf4e029cdf4b425f70236c
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2022-09-17 20:14:23 +0800

    Modify fullscreen on AndroidX

--- app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
+++ app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
@@ -1,29 +1,43 @@
 package com.aldringames.scjd.sbsp.common;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActiviy;
 
 public class BaseActivity extends AppCompatActivity {
-
+    
+    private View mDecorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	overridePendingTransition(0, 0);
 	
-	hideSystemBars();
+	mDecorView = getWindow().getDecorView();
+	mDecorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+	    @Override
+	    public void onSystemUiVisibilityChange(int visibility) {
+		if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+		hideSystemUI();
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+	if (hasFocus) {
+	    hideSystemBars();
+	}
     }
 
     private void hideSystemBars() {
-	WindowInsetsControllerCompat windowInsetsController = ViewCompat.getWindowInsetsController(getWindow().getDecorView());
-	if (windowInsetsController == null) {
-	    return;
-	}
-	windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-  );
-	windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
-}
+	decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 
 }
