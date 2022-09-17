b750a1d6fb10deebaa538624d7a6f40df01d8f5b
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2022-09-17 19:15:01 +0800

    Fix protected void

--- app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
+++ app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	overridePendingTransition(0, 0);
     }
