b8ad9721adbcb493604eca794d8190a6b989144e
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2022-09-17 19:35:04 +0800

    Using fulkscren as immersive mode

--- app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
+++ app/src/main/java/com/aldringames/scjd/sbsp/common/BaseActivity.java
@@ -2,11 +2,27 @@ package com.aldringames.scjd.sbsp.common;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class BaseActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	overridePendingTransition(0, 0);
+	
+	hideSystemBars();
     }
+
+    private void hideSystemBars() {
+	WindowInsetsControllerCompat windowInsetsController = ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+	if (windowInsetsController == null) {
+	    return;
+	}
+	windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+  );
+	windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+}
+
 }
